22370ddd0774aedd06be16c33fcc26fe180360a7
AIT OUASAA MOHAMED <m.aitouasaa@gmail.com>
2019-10-17 13:48:19 +0200

    TP - Javascript partie 1 q 1

---
+++ MonCV/js/javascript.js
@@ -0,0 +1,5 @@
+
+function fonction(x) {
+   //x.parentNode.parentNode.nextElementSibling.style.display= "block" ;
+   x.parentNode.parentNode.nextElementSibling.children[1].style.display ="block";
+}
