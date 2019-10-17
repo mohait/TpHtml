b34cc3aceaa3701a900657c3f91fe0a96319cf78
AIT OUASAA MOHAMED <m.aitouasaa@gmail.com>
2019-10-17 17:26:38 +0200

    TP- Javascript question 3

--- MonCV/js/javascript.js
+++ MonCV/js/javascript.js
@@ -1,14 +1,40 @@
 
 function fonction(x) {
-
+    var ele = x.parentNode.parentNode.nextElementSibling;
    var list = document.getElementsByClassName("formation");
    
-   for (i=0 ; i<5 ; i++){
+   for (i=0 ; i<list.length ; i++){
        if( list[i].style.display=="block"){
            list[i].style.display="none";
        }
    }
-   x.parentNode.parentNode.nextElementSibling.children[1].style.display ="block";
+   ele.children[1].style.display ="block";
+   var max = ele.children[1].offsetHeight ;
+   ele.children[1].style.display ="none";
+   ele.children[1].style.height ="20px";
+   ele.children[1].style.display ="block";
+   timedCount();
+
+
+
+
+   
+   var h = 28;
+   function timedCount() {
+    
+       ele.children[1].style.height = h +'px';
+       h = h+20 ;
+    
+    if (h >  max ) {
+        console.log(i);
+        clearTimeout(t);
+        return;
+    }
+    t = setTimeout(function () {
+        timedCount()
+    }, 100);
+}
+
 
 }
 
